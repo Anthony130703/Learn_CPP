3b30484b6b497f57d10ec4f5278a1c4c74419079
Anthony <tonynarciso2003@gmail.com>
2025-09-15 21:32:09 -0500

    modificandoo el archivo operadores

---
+++ operadores.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main()
+{
+    //Aritmeticos
+    //suma -> a + b 
+    // 5 + 2
+
+    std::cout << "(5 + 2) = " << (5 + 2) << std::endl;
+
+    //suma -> a - b 
+    // 5 - 2
+    
+    std::cout << "(5 - 2) = " << (5 - 2) << std::endl;
+
+    //suma -> a * b 
+    // 5 * 2
+    
+    std::cout << "(5 * 2) = " << (5 * 2) << std::endl;
+
+    //suma -> a / b 
+    // 5 / 2
+    
+    std::cout << "(5 / 2) = " << (5 / 2) << std::endl;
+
+    //suma -> a % b 
+    // 5 % 2
+    
+    std::cout << "(5 % 2) = " << (5 % 2) << std::endl;
+
+    return 0;
+}
