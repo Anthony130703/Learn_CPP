f495f64841a616781b1fc06c1b3db0e5daf18d65
Anthony <tonynarciso2003@gmail.com>
2025-09-16 15:44:03 -0500

    I'm uploding  new files

--- operadores.cpp
+++ operadores.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bitset>
 
 int main()
 {
@@ -27,6 +28,12 @@ int main()
     // 5 % 2
     
     std::cout << "(5 % 2) = " << (5 % 2) << std::endl;
+    //Operador AND
+    std::cout << "(5 & 3) = " << (5 & 3) << std::endl;
+    std::cout << "(5 & 3) = " << std::bitset<4>(5 & 3) << std::endl;
+    //Operador OR
+    std::cout << "(5 | 3) = " << (5 | 3) << std::endl;
+    std::cout << "(5 | 3) = " << std::bitset<4>(5 | 3) << std::endl;
 
     return 0;
 }
